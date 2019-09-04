fd81b20dc11e12109c732792e69b0172dd093dc1
galvarado2015519 <galvarado-2015519@kinal.edu.gt>
2019-09-04 11:05:45 -0600

    Se agregaron los escaners a la clase principal

--- ProyectoColegio/src/main/java/org/colegitoSA/sistema/Principal.java
+++ ProyectoColegio/src/main/java/org/colegitoSA/sistema/Principal.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.colegitoSA.sistema;
 
-/**
- *
- * @author programacion
- */
+import java.util.Scanner;
+import org.colegitoSA.dao.AlumnoJpaController;
+import org.colegitoSA.dominio.Alumno;
+
+
 public class Principal {
-    
+    private static Scanner sc = new Scanner(System.in);
+        private static Scanner salumno = new Scanner(System.in);
+        private static Alumno A;
+        private static AlumnoJpaController alumnoDao = new AlumnoJpaController();
+        private static int op, id;
+        
+        public static void main (String [] args){
+            
+                   
+        }
 }
