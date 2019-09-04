9255e7b91ed9b4b4fdf88a42ab2f257f3ff550e7
mneal2015496 <mneal-2015496@kinal.edu.gt>
2019-09-04 11:13:41 -0600

    Creación del menu y metodos de clase

--- ProyectoColegio/src/main/java/org/colegitoSA/sistema/Principal.java
+++ ProyectoColegio/src/main/java/org/colegitoSA/sistema/Principal.java
@@ -14,7 +14,30 @@ public class Principal {
         private static int op, id;
         
         public static void main (String [] args){
-            
-                   
+            System.out.println("--------------------------------------------");
+            System.out.println("-------------------Menu---------------------");
+            System.out.println("--------------------------------------------");
+            System.out.println("------------1. Mostrar Alumno---------------");
+            System.out.println("------------2. Agregar Alumno---------------");
+            System.out.println("-------3. Actualizar Datos del Alumno-------");
+            System.out.println("-------4. Eliminar Datos de un Alumno-------");
+            System.out.println("------------5. Listar Alumnos---------------");
+            System.out.println("--------------------------------------------");
+            System.out.println("--------Por favor Ingrese una opción--------");
+            System.out.println("--------------------------------------------");               
         }
+        
+        public static Alumno obtenerAlumno(){
+	System.out.println("Ingrese el código del Alumno");
+	id= sc.nextInt();
+	A = alumnoDao.buscarAlumno(id);
+        return A;
+        }
+
+        private static Alumno getAlumno(){
+            System.out.println("Ingrese x atributos");
+            Alumno A =  new Alumno(sc.nextInt(), salumno.nextLine(), salumno.nextLine(), salumno.nextLine(), salumno.nextLine(), salumno.nextLine(), sc.nextInt());
+            return A;
+}
+        
 }
